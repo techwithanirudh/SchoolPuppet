3ef17768954372c3f26bde245f699c9cf9890fd9
Anirudh <70373803+techwithanirudh@users.noreply.github.com>
2022-02-26 23:13:57 +0530

    Create main.sh

---
+++ main.sh
@@ -0,0 +1,9 @@
+# With my big brain, i see how to do it now!
+# post and tutorial: 
+# repl.it/talk/share/This-time-we-are-getting-puppeteer/28380
+# omg it actually looks like what it looks like on my desktop
+## npm init
+# npm install axios
+# npm install puppeteer --save && npm start
+npm start
+# node index.js
